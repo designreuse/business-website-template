0922372335e700d88e1509604ab331bd86b113da
will van wart <william@wvwproductions.com>
2016-12-28 10:50:26 -0500

    add dynamic nav bar w/ custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
+  console.log("ready")
   "use strict";
   $(window).scroll(function(){
+    console.log("scroll")
     "use strict";
 
     if($(window).scrollTop() < 80 ) {
@@ -8,10 +10,20 @@ $(document).ready(function(){
           'margin-top': '-100px',
           'opacity': '0'
         });
+
+        $('.navbar-default').css({
+          'background-color': 'rgba(59,59,59,0)'
+        });
+
       } else {
         $('.navbar').css({
-          'margin-top': '-0px',
+          'margin-top': '0px',
           'opacity': '1'
+        });
+
+        $('.navbar-default').css({
+          'background-color': 'rgba(59,59,59,0.85)',
+          'border-color': '#444'
         });
       }
   });
