f7eb5f6815f3090d21dc6b26f8cf08336a8033a1
will van wart <william@wvwproductions.com>
2016-12-29 19:12:32 -0500

    add smooth scroll

--- js/custom.js
+++ js/custom.js
@@ -25,6 +25,30 @@ $(document).ready(function(){
           'background-color': 'rgba(59,59,59,0.85)',
           'border-color': '#444'
         });
+        $('.navbar-brand img').css({
+          'height': '50px',
+          'padding-top': '0px'
+        });
+        $('navbar-nav > li > a').css({
+          'padding-top': '15px'
+        })
       }
+
+  });
+});
+
+$(document).ready(function(){
+  'use strict';
+    $('.nav-item, #scroll-to-top').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
   });
 });
